9265cb0b5b07dc1aa6309353d3e0d22bd336098d
Rishabh-mac <raj.rishabh01@gmail.com>
2022-11-18 16:13:11 -0500

    A8 Part 5

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import HelloController from './controllers/hello-controller.js'
+import TuitsController from './controllers/tuits/tuits-controller.js';
 import UserController from './controllers/users/users-controller.js';
 
 const app = express()
 
 app.use(express.json());
+
+TuitsController(app);
 HelloController(app);
 UserController(app);
 
