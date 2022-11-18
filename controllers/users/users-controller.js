c1275d5d18dca755e5decfb1d313f21d0d69c07a
Rishabh-mac <raj.rishabh01@gmail.com>
2022-11-18 13:51:19 -0500

    A8 part4

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -5,6 +5,9 @@ let users = people;
 const UserController = (app) => {
     app.get('/api/users', findUsers)
     app.get('/api/users/:uid', findUsersById)
+    app.post('/api/users', createUser)
+    app.delete('/api/users/:uid', deleteUser)
+    app.put('/api/users/:uid', updateUser)
 }
 
 const findUsers = (req,res) => {
@@ -21,6 +24,30 @@ const findUsersById = (req,res) => {
     const userId = req.params.uid;
     const user = users.filter(u => u._id === userId);
     res.json(user)
+}
+
+const createUser = (req,res) => {
+    const newUser = req.body
+    newUser._id = (new Date()).getTime() + '';
+    users.push(newUser);
+    res.json(newUser);
+}
+
+const deleteUser = (req, res) => {
+    const userId = req.params.uid
+    users = users.filter(usr => usr._id !== userId);
+    res.send(200)
+}
+
+const updateUser = (req, res) => {
+    const userToUpdate = req.params.uid
+    const updates = req.body
+    users = users.map((usr) =>
+        usr._id === userToUpdate ? 
+        {...usr, ...updates} :
+        usr
+    );
+    res.sendStatus(200);
 }
 
 export default UserController;
