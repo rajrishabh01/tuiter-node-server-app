198bec92e375b99c8f1121df31f6e36a42e129d4
Rishabh-mac <raj.rishabh01@gmail.com>
2022-11-28 00:28:23 -0500

    Mongodb changes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import HelloController from './controllers/hello-controller.js'
 import TuitsController from './controllers/tuits/tuits-controller.js';
 import UserController from './controllers/users/users-controller.js';
 import cors from 'cors'
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://localhost:27017/tuiter');
 
 const app = express()
 
